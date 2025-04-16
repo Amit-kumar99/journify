38730f7c7fa97568c6b8c80c0207d571900b8e7c
Amit-kumar99 <amit.kumar.16208932@gmail.com>
2025-04-16 09:37:53 +0530

    use @Service annotations for service classes

--- src/main/java/com/edigest/journalApp/service/JournalEntryService.java
+++ src/main/java/com/edigest/journalApp/service/JournalEntryService.java
@@ -5,16 +5,14 @@ import com.edigest.journalApp.entity.User;
 import com.edigest.journalApp.repository.JournalEntryRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 
-@Component
+@Service
 @Slf4j
 public class JournalEntryService {
 
